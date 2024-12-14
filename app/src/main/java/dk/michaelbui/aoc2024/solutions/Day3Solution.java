0eb807dc51b1463215330a07eea475ddb7f03ae8
Michael Bui <michaelbui99@hotmail.com>
2024-12-14 20:30:30 +0100

    Solve Day 3 Puzzle 1

--- app/src/main/java/dk/michaelbui/aoc2024/solutions/Day3Solution.java
+++ app/src/main/java/dk/michaelbui/aoc2024/solutions/Day3Solution.java
@@ -5,6 +5,8 @@ import dk.michaelbui.aoc2024.models.instruction.InstructionParser;
 import dk.michaelbui.aoc2024.models.instruction.MulInstruction;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day3Solution implements Solution {
     private PuzzleInputReader inputReader = new PuzzleInputReader("input_day3.txt");
@@ -13,12 +15,20 @@ public class Day3Solution implements Solution {
     public void solvePuzzleOne() {
         List<String> input = inputReader.read();
 
-        InstructionParser parser = new InstructionParser(input.get(0));
+        Pattern pattern = Pattern.compile("mul\\([0-9]+,[0-9]+\\)");
+        Matcher matcher = pattern.matcher(String.join("", input));
 
+        StringBuilder rawMulInstruction = new StringBuilder();
+
+        while (matcher.find()) {
+            rawMulInstruction.append(matcher.group());
+        }
+
+        InstructionParser parser = new InstructionParser(rawMulInstruction.toString());
         int res = parser.parse()
                 .stream()
-                .map(MulInstruction::evaluate)
-                .reduce(0, (total, next) -> total += next);
+                .mapToInt(MulInstruction::evaluate)
+                .sum();
 
         System.out.printf("Day 3 Puzzle 1 answer: %s%n", res);
     }
--- app/src/test/java/dk/michaelbui/aoc2024/models/InstructionParser.java
+++ app/src/test/java/dk/michaelbui/aoc2024/models/InstructionParser.java
@@ -24,7 +24,7 @@ public class InstructionParser {
     }
 
     @Test
-    public void parse_validMulInstructions_returnsListOfMulInstructions(){
+    public void parse_validMulInstructions_returnsListOfMulInstructions() {
         // Arrange
         String source = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         dk.michaelbui.aoc2024.models.instruction.InstructionParser parser = new dk.michaelbui.aoc2024.models.instruction.InstructionParser(source);
@@ -38,6 +38,20 @@ public class InstructionParser {
         assertEquals("mul(5,5)", instructions.get(1).toString());
         assertEquals("mul(11,8)", instructions.get(2).toString());
         assertEquals("mul(8,5)", instructions.get(3).toString());
+        assertEquals(161, instructions.stream().mapToInt(MulInstruction::evaluate).sum());
+    }
+
+    @Test
+    public void parse_validMulInstructions_returnsListOfMulInstructions2() {
+        // Arrange
+        String source = "where())what()@)select()why()?mul(371,776)/%how()'~+:how()mul(977,266)@$@mul(749,170)how()";
+        dk.michaelbui.aoc2024.models.instruction.InstructionParser parser = new dk.michaelbui.aoc2024.models.instruction.InstructionParser(source);
+
+        // Act
+        List<MulInstruction> instructions = parser.parse();
+
+        // Assert
+        assertEquals(3, instructions.size());
     }
 
     @Test
